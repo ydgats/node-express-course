3347165a72e042cf390d70ee2c0065ab7b9b12fa
Yiding Zhao <ydzhao380@gmail.com>
2020-03-22 18:42:23 -0400

    add login POST

--- server.js
+++ server.js
@@ -1,18 +1,50 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 const mockUserData = [
     {name: 'Mark'},
-    {name: 'Jill'}
+    {name: 'Jill'},
 ]
 
-app.get('/users/:id', function(req, res) {
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'get users',
+        users: mockUserData,
+    })
+})
+
+app.get('/user/:id', function(req, res) {
     console.log(req.params.id)
     res.json({
         success: true,
-        message: 'sucessefully send users',
-        users: mockUserData
+        message: 'get one user',
+        user: mockUserData[0],
     })
+})
+
+app.post('/login',function() {
+    const username = req.body.username
+    const password = req.body.password
+
+    const mockUsername = 'John'
+    const mockPassword = 'Doe'
+
+    if (username === mockUsername && password === mockPassword) {
+        res.json({
+            sucess: true,
+            message: 'password and username pass!',
+            token: 'an encrypted token',
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match',
+        })
+    }
 })
 
 app.listen(8000, function() {
