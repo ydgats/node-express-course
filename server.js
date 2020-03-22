d6d568942e8eafbe6a7f4ca465dba15019098760
Yiding Zhao <ydzhao380@gmail.com>
2020-03-22 18:33:41 -0400

    add first GET request

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name: 'Mark'},
+    {name: 'Jill'}
+]
+
+app.get('/users', function(req, res) {
+    res.json({
+        success: true,
+        message: 'sucessefully send users',
+        users: mockUserData
+    })
+})
+
 app.listen(8000, function() {
     console.log('server is running');
 })
